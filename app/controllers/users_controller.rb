3f05fe6a4b109ee6f9e68354809e8c55ef33899e
WilsonReese <reese@chicagobooth.edu>
2020-11-04 17:58:59 +0000

    Update user seems to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,5 +26,21 @@ class UsersController < ApplicationController
     # render({ :template => "user_templates/add.html.erb" })
     redirect_to("/users/" + a_new_user.username)
   end
+
+  def update
+    # Parameters: {"query_username"=>"qwerty", "path_username"=>"qwert"}
+    updated_username = params.fetch("query_username")
+    current_username = params.fetch("path_username")
+
+    matching_users = User.where({ :username => current_username})
+    the_user = matching_users.first
+
+    the_user.username = updated_username
+
+    the_user.save
+
+    # render({ :template => "user_templates/update.html.erb" })
+    redirect_to("/users/" + the_user.username)
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
   get("/insert_user_record", { :controller => "users", :action => "add"})
+  get("/update_user/:path_username", { :controller => "users", :action => "update" })
   
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
