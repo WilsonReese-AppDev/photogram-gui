badff459adbfd7cf3931c8acb30aab083a75b88b
WilsonReese <reese@chicagobooth.edu>
2020-11-04 00:12:09 +0000

    Added delete photo and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+  
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:path_username", { :controller => "users", :action => "show" })
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show" })
+
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "remove" })
 end
